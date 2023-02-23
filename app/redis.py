a410331d182734cbabb1018acee6c788c925e0f0
mys1erious <subtletyzeros@gmail.com>
2023-02-24 00:43:22 +0100

    fix: removed test code and added redis_connect()

--- app/redis.py
+++ app/redis.py
@@ -1,31 +1,13 @@
-from datetime import timedelta
-from typing import Optional
-
 from redis import asyncio as aioredis
-from pydantic import BaseModel
+from app.config import settings
 
 
 redis_client: aioredis.Redis = None  # type: ignore
 
 
-class RedisSchema(BaseModel):
-    key: bytes | str
-    value: bytes | str
-    ttl: Optional[int | timedelta]
-
-
-async def set_key(redis_data: RedisSchema, is_transaction: bool = False) -> None:
-    async with redis_client.pipeline(transaction=is_transaction) as pipe:
-        await pipe.set(redis_data.key, redis_data.value)
-        if redis_data.ttl:
-            await pipe.expire(redis_data.key, redis_data.ttl)
-
-        await pipe.execute()
-
-
-async def get_by_key(key: str) -> Optional[str]:
-    return await redis_client.get(key)
-
-
-async def delete_by_key(key: str) -> None:
-    return await redis_client.delete(key)
+async def redis_connect():
+    pool = aioredis.ConnectionPool.from_url(
+        settings.REDIS_URL,
+        max_connections=10
+    )
+    return await aioredis.Redis(connection_pool=pool)
